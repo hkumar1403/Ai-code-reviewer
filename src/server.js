f4c24b44bf8b9d057874538695942f6dbba5c187
hkumar1403 <kumarhemant1403@gmail.com>
2026-05-04 16:21:28 +0530

    added backend url for cors

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://ai-code-reviewer-frontend-xi.vercel.app",
   }),
 );
 app.use(authRouter);
